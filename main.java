37f8461d35c84175338790c66a4c15a847f5bf84
Nachetto <itsnachetto@gmail.com>
2023-02-16 20:47:00 +0100

    Prueba de cambios Ignacio Llorente

---
+++ main.java
@@ -0,0 +1,7 @@
+import java.io.IOException;
+
+public class main {
+    public static void main(String[] Args) throws IOException {
+        System.out.println("hey! estamos con la practica de manejo basico de git, Ignacio Llorente");
+    }
+}
