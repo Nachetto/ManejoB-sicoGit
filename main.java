7e1367b32cec51edd9290b2361e484cd715e7d86
Nachetto <115470440+Nachetto@users.noreply.github.com>
2023-02-16 20:57:56 +0100

    Cambios realizados por otro trabajador

--- main.java
+++ main.java
@@ -3,5 +3,6 @@ import java.io.IOException;
 public class main {
     public static void main(String[] Args) throws IOException {
         System.out.println("hey! estamos con la practica de manejo basico de git, Ignacio Llorente");
+        System.out.println("Esta línea de código representa los cambios hechos por otro trabajador");
     }
 }
